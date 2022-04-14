7a9e7d851a67bafd1511d5a67ddc539de130fa01
seyfgoumeida <goumeidaahmedseyfeddine@gmail.com>
2022-04-14 22:51:49 +0200

    [ADD] CreationDate and visualisation

--- src/main/kotlin/Account.kt
+++ src/main/kotlin/Account.kt
@@ -1,8 +1,12 @@
+import java.time.LocalDate
+import java.util.Date
+
 data class Account(
-val id: Int,
-val firstName: String,
-val lastName: String,
-val type: String,
-val interestRate: Double,
-val balance: Int
+    val id: Int,
+    val creationDate : LocalDate,
+    val firstName: String,
+    val lastName: String,
+    val type: String,
+    val interestRate: Double,
+    val balance: Int
 )
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,21 +3,25 @@ import java.io.File
 import java.io.InputStreamReader
 import java.io.BufferedReader
 import java.util.ArrayList
-
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun printAccount(bankAccount: Account) {
-    println("""
-			${bankAccount.id}\t
-			${bankAccount.firstName}\t\t
-			${bankAccount.lastName}\t\t
-			${bankAccount.type}\t\t
-			${bankAccount.interestRate}\t\t\t
-			${bankAccount.balance}
-	""")
+
+    println(
+            bankAccount.id.toString() + "\t \t \t"+
+                    bankAccount.creationDate.toString()+ "\t \t \t"+
+                    bankAccount.firstName+ "\t \t \t"+
+                    bankAccount.lastName+ "\t \t \t"+
+                    bankAccount.type+ "\t \t \t"+
+                    bankAccount.interestRate+ "\t \t \t"+
+                    bankAccount.balance
+    )
 }
 
 fun printAccountHead() {
-    println("Number\tName\t\t\tType\t\tInterest Rate\t\tBalance")
+    println("Id\t\t\tcreationDate\t\tFirstName\t\tLastName\t\ttype\t\tRate\t\tBalance")
+
 }
 
 fun printAccounts(bankAccounts: List<Account>) {
@@ -35,12 +39,14 @@ fun readAccounts(): ArrayList<Account> {
         val line = it.split(", ")
         if(line.size > 1) {
             val id = line[0].removeSurrounding("\"").toInt()
-            val firstname = line[1].removeSurrounding("\"") + ", " + line[2].removeSurrounding("\"")
-            val lastname = line[1].removeSurrounding("\"") + ", " + line[2].removeSurrounding("\"")
-            val type = line[3].removeSurrounding("\"")
-            val interestRate = line[4].toDouble()
-            val balance = line[4].substring(0, line[4].length - 1).toInt()
-            val bankAccount = Account(id, firstname,lastname, type, interestRate, balance)
+            val date = line[1].removeSurrounding("\"")
+            val creationDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE)
+            val firstname = line[2].removeSurrounding("\"")
+            val lastname = line[3].removeSurrounding("\"")
+            val type = line[4].removeSurrounding("\"")
+            val interestRate = line[5].toDouble()
+            val balance = line[6].substring(0, line[5].length - 1).toInt()
+            val bankAccount = Account(id,creationDate, firstname,lastname, type, interestRate, balance)
 
             bankAccounts.add(bankAccount)
         }
@@ -53,29 +59,62 @@ fun printBanner() {
     println("Bank Account Management Main Menu:")
     println()
     println("Please select an option from the following menu:")
-    println("\t • (E)nter New Accounts")
-    println("\t • (C)heck Account Record")
-    println("\t • (S)how Sorted Account List")
-    println("\t •  E(x)it")
+    println("\t • 1 - Enter New Accounts")
+    println("\t • 2 - Check Account Record")
+    println("\t • 3 - Show Sorted Account List")
+    println("\t • 4 - Exit")
 }
 
 fun newAccount(input: BufferedReader): Account {
     input.readLine()
     println("Enter New Account Data:")
     print("\t • Account No.:")
-    val number = input.readLine().toInt()
-    print("\t • Name:")
+    val id = input.readLine().toInt()
+
+    val creationDate = LocalDate.now()
+    println("\t • Date of creation :$creationDate")
+
+    print("\t • FirstName:")
     val firstname = input.readLine()
-    print("\t • Name:")
+
+    print("\t • LastName: ")
     val lastname = input.readLine()
-    print("\t • Account Type:")
-    val type = input.readLine()
+
+
+    print("\t • Account Type: (1)Basic  (2)Student  (3)Individual (4)Joint")
+    var type = input.readLine()
+
+    while (type!="1" && type!="2"&& type!="3"&& type!="4"){
+            println("type is out of choices please choose :")
+            print("\t • Account Type: (1)Basic  (2)Student  (3)Individual (4)Joint")
+        type = input.readLine()
+    }
+
+    when (type) {
+        "1" -> {
+            println("\t • type : Basic")
+            type ="Basic"
+        }
+        "2" -> {
+            println("\t • type : Student")
+            type ="Student"
+        }
+        "3" -> {
+            println("\t • type : Individual")
+            type ="Individual"
+        }
+        "4" -> {
+            println("\t • type : Joint")
+            type ="Joint"
+        }
+    }
+
     print("\t • Interest Rate:")
     val interestRate = input.readLine().toDouble()
     print("\t • Balance:")
     val balance = input.readLine().toInt()
 
-    return Account(number, firstname,lastname, type, interestRate, balance)
+    return Account(id, creationDate,firstname,lastname, type, interestRate, balance)
 }
 
 fun main(args: Array<String>) {
